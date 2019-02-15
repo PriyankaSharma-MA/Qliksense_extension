cc50a60a90326f6a4bf3d7319126d77e01621434
priyanka sharma <priyanka.sharma@mediaagility.com>
2019-02-15 16:09:08 +0530

    commit_15022019

--- applicationlink.js
+++ applicationlink.js
@@ -1,6 +1,6 @@
 // JavaScript
 
-var DashboardsList='4f2a8d2c-9678-48d4-9eae-60dd3dc54241'
-var DashboardFilter='fc693cd3-764e-486a-85b7-fc81a870b8fc'
-var DefaultLink='17a91466-cc75-46bb-b057-7c4c95725750'
+var DashboardsList='Dashboardslist.qvf'
+var DashboardFilter='DashboardFilter.qvf'
+var DefaultLink='DefaultLink.qvf'
 
